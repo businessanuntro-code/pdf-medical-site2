cb78e922b5602aa94d3471c8f35eff5331bc55f5
Diaconu Daniel <businessanuntro@gmail.com>
2026-07-16 15:32:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,11 @@
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+EXTRACT_FOLDER = os.path.join(BASE_DIR, "extracted")
+PROCESSED_FOLDER = os.path.join(BASE_DIR, "processed")
+OUTPUT_FOLDER = os.path.join(BASE_DIR, "output")
+DATABASE_FOLDER = os.path.join(BASE_DIR, "database")
+
+ALLOWED_EXTENSIONS = {"zip"}
