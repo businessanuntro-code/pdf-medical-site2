2f71bf10d4b0d7b13dd7f0a88fe4bdee12c41e37
Diaconu Daniel <businessanuntro@gmail.com>
2026-07-16 15:54:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 import config
+from parser.analyzer import analyze_zip
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.config["UPLOAD_FOLDER"] = config.UPLOAD_FOLDER
 app.config["MAX_CONTENT_LENGTH"] = config.MAX_CONTENT_LENGTH
 
 
-# Creează folderele necesare dacă nu există
+# Creare foldere necesare
 for folder in [
     config.UPLOAD_FOLDER,
     config.EXTRACT_FOLDER,
@@ -23,11 +24,14 @@ for folder in [
     os.makedirs(folder, exist_ok=True)
 
 
+
 def allowed_file(filename):
     return (
         "." in filename
-        and filename.rsplit(".", 1)[1].lower() in config.ALLOWED_EXTENSIONS
+        and filename.rsplit(".", 1)[1].lower()
+        in config.ALLOWED_EXTENSIONS
     )
+
 
 
 @app.route("/")
@@ -35,9 +39,11 @@ def home():
     return render_template("home.html")
 
 
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
 
 
 @app.route("/upload", methods=["GET", "POST"])
@@ -49,37 +55,57 @@ def upload():
             flash("Nu a fost selectată nicio arhivă.")
             return redirect(request.url)
 
+
         file = request.files["revista"]
+
 
         if file.filename == "":
             flash("Nu a fost selectat niciun fișier.")
             return redirect(request.url)
 
+
+
         if not allowed_file(file.filename):
             flash("Este permis doar formatul ZIP.")
             return redirect(request.url)
 
+
+
         filename = file.filename
+
 
         save_path = os.path.join(
             app.config["UPLOAD_FOLDER"],
             filename
         )
 
+
+        # Salvare arhivă
         file.save(save_path)
+
+
+
+        # Analiză inițială ZIP
+        analysis = analyze_zip(save_path)
+
+
 
         return render_template(
             "upload.html",
             uploaded=True,
-            filename=filename
+            filename=filename,
+            analysis=analysis
         )
 
+
     return render_template("upload.html")
+
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("404.html"), 404
+
 
 
 if __name__ == "__main__":
