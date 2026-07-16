d4c527fa0fb2b534b7c946a5ce8eb3179ad0db2c
Diaconu Daniel <businessanuntro@gmail.com>
2026-07-16 16:55:04 +0300

    Update extractor.py

--- parser/extractor.py
+++ parser/extractor.py
@@ -1,1 +1,23 @@
+import os
+import zipfile
 
+
+def extract_zip(zip_path, extract_folder):
+    """
+    Dezarhivează revista în folderul de lucru.
+    Returnează calea către folderul extras.
+    """
+
+    os.makedirs(
+        extract_folder,
+        exist_ok=True
+    )
+
+    with zipfile.ZipFile(zip_path, "r") as archive:
+
+        archive.extractall(
+            extract_folder
+        )
+
+
+    return extract_folder
