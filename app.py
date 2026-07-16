135afef1e1a18ffbd6c518fcaa924b07452e1ae8
Diaconu Daniel <businessanuntro@gmail.com>
2026-07-16 14:49:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
