665dee11a346ff4de7dd471c8a718b649a732bfd
Diaconu Daniel <businessanuntro@gmail.com>
2026-07-20 16:07:14 +0300

    Update parser.py

--- app/parser.py
+++ app/parser.py
@@ -135,6 +135,10 @@ def parse_xml(path):
                 if text:
                     refs.append(f"{nr}{text}")
 
+    print("BIBLIOGRAFIE EXTRASA:")
+    for r in refs:
+        print(r)
+
     data["bibliografie"] = "\n".join(refs)
 
     return data
