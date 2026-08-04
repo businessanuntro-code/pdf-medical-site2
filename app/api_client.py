a2eb1051d493508b14b1fb52319e907136daa807
Diaconu Daniel <businessanuntro@gmail.com>
2026-08-04 12:56:36 +0300

    Update api_client.py

--- app/api_client.py
+++ app/api_client.py
@@ -1,1 +1,15 @@
-publish_article(data)
+import requests
+
+
+def publish_article(data):
+
+    url = "URL_API_IMPORT"
+
+    response = requests.post(
+        url,
+        json=data
+    )
+
+    response.raise_for_status()
+
+    return response.json()
