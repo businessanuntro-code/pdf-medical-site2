3bc58eb71f407a4c87e9f579e08410093877430c
Diaconu Daniel <businessanuntro@gmail.com>
2026-08-04 13:05:41 +0300

    Update api_client.py

--- app/api_client.py
+++ app/api_client.py
@@ -5,9 +5,15 @@ def publish_article(data):
 
     url = "https://diaconu-daniel.ro/api/import.php"
 
+    headers = {
+        "X-API-Key": "MEDICHUB_SECRET_2026"
+    }
+
     response = requests.post(
         url,
-        json=data
+        json=data,
+        headers=headers,
+        timeout=30
     )
 
     response.raise_for_status()
